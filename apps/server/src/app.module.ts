6b388c22a368c86e2d030ee3c6ee3444fe685596
victorgarcia2006 <vgarciac2100@alumno.ipn.mx>
2025-11-07 14:08:57 -0600

    :sparkles: Conexion de DB

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -23,11 +23,8 @@ import { Cuenta } from './cuentas/entities/cuenta.entity';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
+        url: configService.get<string>('DB_URL'),
+        ssl: { rejectUnauthorized: false },
         entities: [User, Poliza, Reporte, Movimiento, Cuenta], 
         synchronize: true, 
       }),
